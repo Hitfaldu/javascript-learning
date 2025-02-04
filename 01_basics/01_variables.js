dcb534acf6d5eceec5f0d3769b5faf3a13fc6b40
Hitfaldu <184423521+Hitfaldu@users.noreply.github.com>
2025-02-04 22:20:14 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 124578
+let accountEmail = "hitfaldu@google.com"
+var accountPassword = "123456"
+accountCity = "Rajkot"
+let accountState;
+// accountId = 565656
+
+accountEmail = "hitesh@google.com"
+accountCity = "bengluru"
+accountPassword = "1211212"
+console.log(accountId);
+
+console.table([accountId, accountCity, accountEmail, accountPassword, accountState])
